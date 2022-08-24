0403f070fc81d5dce4af26fb0d352bf41225ecb0
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-24 11:22:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    if (activity === '' ){
+        console.log (`This Saturday, I want to ${activity}!`);
+    } else {return (`This Saturday, I want to ${activity}!`);
+}
+}
+
+
+const mondayWork = function (activity = 'go to the office'){
+    if (activity === ''){
+        console.log (`This Monday, I will ${activity}.`);
+    } else {return (`This Monday, I will ${activity}.`);
+};
+}
+
+
+function wrapAdjective (highlight){
+    const emphatic = function (result){
+        return `You are ${highlight}${result}${highlight}!`;
+    }
+    return emphatic;
+}
